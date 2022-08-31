c2123a24b2aa1a6d2b9b3981ef1396b1973dbc55
Isaac Hussain <isaaclearns58@gmail.com>
2022-08-31 22:27:38 +0300

    add dark mode

--- script.js
+++ script.js
@@ -2,20 +2,9 @@ const addBtn = document.querySelector(".add-btn");
 const modalContainer = document.querySelector(".modal-container");
 const submitBtn = document.querySelector(".submit-btn");
 const checkbox = document.querySelector("#checkbox");
-const header = document.querySelector("header");
-const cards = document.querySelectorAll(".card");
-const cardBtns = document.getElementsByClassName("card-btns");
-const allElements = document.getElementsByClassName("dark-mode");
 
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-  header.classList.toggle("dark");
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].classList.toggle("dark");
-  }
-  for (let i = 0; i < cardBtns.length; i++) {
-    cardBtns[i].classList.toggle("dark");
-  }
+  document.body.classList.toggle("dark-theme");
 });
 
 addBtn.addEventListener("click", () => {
@@ -25,4 +14,3 @@ addBtn.addEventListener("click", () => {
 submitBtn.addEventListener("click", () => {
   modalContainer.classList.remove("show");
 });
-f;
