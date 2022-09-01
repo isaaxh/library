d67703b632833acfd1439fbc7621ca579ae18b88
Isaac Hussain <isaaclearns58@gmail.com>
2022-09-01 12:24:00 +0300

    Add darkmode

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+const log = console.log;
+
 const addBtn = document.querySelector(".add-btn");
-const modalContainer = document.querySelector(".modal-container");
+const modalContainer = document.querySelector("#modal-container");
 const submitBtn = document.querySelector(".submit-btn");
 const checkbox = document.querySelector("#checkbox");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const readStatus = document.querySelector("#read-status");
+
+log(readStatus);
+
+let myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {}
 
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark-theme");
@@ -12,5 +31,9 @@ addBtn.addEventListener("click", () => {
 });
 
 submitBtn.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
+
+modalContainer.addEventListener("click", () => {
   modalContainer.classList.remove("show");
 });
